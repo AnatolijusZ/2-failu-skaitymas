669a4bfeefba20d5d63ba3173d0dd5400cf64feb
AnatolijusZ <zubkovas.a@gmail.com>
2021-08-02 13:45:09 +0300

    fixes

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let tekstas, nuskaitimas;
  
 console.log("start");
  
-nuskaitimas = fs.readFileSync("a.txt", "UTF-8"); 
+nuskaitimas = fs.readFileSync("a.txt", "utf8"); 
 tekstas += nuskaitimas;
 
-nuskaitimas = fs.readFileSync("b.txt", "UTF-8"); 
+nuskaitimas = fs.readFileSync("b.txt", "uf8"); 
 tekstas += nuskaitimas;
 
-nuskaitimas = fs.readFileSync("c.txt", "UTF-8"); 
+nuskaitimas = fs.readFileSync("c.txt", "utf8"); 
 tekstas += nuskaitimas;
 
 console.log(tekstas);
